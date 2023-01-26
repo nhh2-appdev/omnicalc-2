f16a7e10946765820e3a43025a394259462b0d40
nhh2 <nhernand@chicagobooth.edu>
2023-01-26 02:58:54 +0000

    Through Subtract

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
 
+  def homepage
+    render({ :template => "calculation_templates/subtract.html.erb"})
+  end
+
+  def subtract
+    render ({ :template => "calculation_templates/subtract.html.erb"})
+  end
+
+  def wizard_subtract
+    @item1 = params.fetch("first_num").to_f
+    @item2 = params.fetch("second_num").to_f
+    @result = @item1.to_f - @item2.to_f
+
+    render ({ :template => "calculation_templates/subtract_results.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Define the homepage URL
+    get("/", { :controller => "application", :action => "homepage"})
+
+  # Define the /subtract URL
+    get("/subtract", { :controller => "application", :action => "subtract"})
+
+  # Define the /wizard_subtract URL
+    get("/wizard_subtract", { :controller => "application", :action => "wizard_subtract"})
+
 end
