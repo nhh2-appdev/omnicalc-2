1fdd39a0ea8fc8c047890fa0f89b93e3f6367dee
nhh2 <nhernand@chicagobooth.edu>
2023-01-30 03:56:27 +0000

    Final version (non stretch)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/subtract.html.erb"})
   end
 
+# Subtract page
+
   def subtract
     render ({ :template => "calculation_templates/subtract.html.erb"})
   end
@@ -11,9 +13,51 @@ class ApplicationController < ActionController::Base
   def wizard_subtract
     @item1 = params.fetch("first_num").to_f
     @item2 = params.fetch("second_num").to_f
-    @result = @item1.to_f - @item2.to_f
+    @result = @item2.to_f - @item1.to_f
 
     render ({ :template => "calculation_templates/subtract_results.html.erb"})
+  end
+
+# Addition page
+
+  def addition
+    render ({ :template => "calculation_templates/addition.html.erb"})
+  end
+
+  def wizard_add
+    @item1 = params.fetch("first_num").to_f
+    @item2 = params.fetch("second_num").to_f
+    @result = @item1.to_f + @item2.to_f
+
+    render ({ :template => "calculation_templates/addition_results.html.erb"})
+  end
+
+# Multiplication page
+
+  def multiplication
+    render ({ :template => "calculation_templates/multiplication.html.erb"})
+  end
+
+  def wizard_multiply
+    @item1 = params.fetch("first_num").to_f
+    @item2 = params.fetch("second_num").to_f
+    @result = @item1.to_f * @item2.to_f
+
+    render ({ :template => "calculation_templates/multiplication_results.html.erb"})
+  end
+
+# Division page
+
+  def division
+    render ({ :template => "calculation_templates/division.html.erb"})
+  end
+
+  def wizard_divide
+    @item1 = params.fetch("first_num").to_f
+    @item2 = params.fetch("second_num").to_f
+    @result = @item1.to_f / @item2.to_f
+
+    render ({ :template => "calculation_templates/division_results.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,32 @@ Rails.application.routes.draw do
   # Define the homepage URL
     get("/", { :controller => "application", :action => "homepage"})
 
+# Subtract
   # Define the /subtract URL
     get("/subtract", { :controller => "application", :action => "subtract"})
 
   # Define the /wizard_subtract URL
     get("/wizard_subtract", { :controller => "application", :action => "wizard_subtract"})
+
+# Add
+  # Define the /add URL
+    get("/add", { :controller => "application", :action => "addition"})
+
+  # Define the /wizard_add URL
+    get("/wizard_add", { :controller => "application", :action => "wizard_add"})
+
+# Multiply
+  # Define the /add URL
+  get("/multiply", { :controller => "application", :action => "multiplication"})
+
+  # Define the /wizard_multiply URL
+    get("/wizard_multiply", { :controller => "application", :action => "wizard_multiply"})
+
+# Divide
+  # Define the /divide URL
+  get("/divide", { :controller => "application", :action => "division"})
+
+  # Define the /wizard_divide URL
+    get("/wizard_divide", { :controller => "application", :action => "wizard_divide"})
 
 end
